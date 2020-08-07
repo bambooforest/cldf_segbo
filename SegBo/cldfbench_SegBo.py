afd0dd991a6027c25b1e402e116d6fd4dcc23285
bambooforest@gmail.com <bambooforest@gmail.com>
2020-08-07 14:59:47 +0200

    towards hacking together cldf format

--- SegBo/cldfbench_SegBo.py
+++ SegBo/cldfbench_SegBo.py
@@ -19,7 +19,6 @@ class Dataset(BaseDataset):
 
         """
         self.raw_dir.download('https://raw.githubusercontent.com/segbo-db/segbo/master/data/segbo_with_glottolog.csv', 'segbo_with_glottolog.csv')
-        self.raw_dir.download('https://wals.info/feature/1A.tab', '1A.tsv')
 
     def cmd_makecldf(self, args):
         """
@@ -27,15 +26,56 @@ class Dataset(BaseDataset):
 
         >>> args.writer.objects['LanguageTable'].append(...)
         """
-        from csvw.dsv_dialects import Dialect
+
+        # Add components
+        """
+        args.writer.cldf.add_columns(
+            "ValueTable",
+            {"name": "Marginal", "datatype": "boolean"},
+            {"name": "Allophones", "separator": " "},
+            "Contribution_ID",
+        )
+        """
+        # args.writer.cldf.add_component("ValueTable")
+        """
+        args.writer.cldf.add_component("ParameterTable")
+        args.writer.cldf.add_component(
+            "LanguageTable", "Family_Glottocode", "Family_Name"
+        )
+        """
+
+        # Iterate over raw data
+        values = []
+        counter = 1
+
+        # Read raw data
         for row in self.raw_dir.read_csv(
-                '1A.tsv',
+                'segbo_with_glottolog.csv',
                 dicts=True,
-                dialect=Dialect(
-                    skipRows=5,  # Ignore the citation info on top
-                    skipBlankRows=True,
-                    delimiter='\t',
-                )
+        ):
+            print(row)
+            values.append(
+                {
+                    "ID": str(counter),
+                    "Language_ID": row['Glottocode'],
+                    "Value": row['BorrowedSound'],
+                    "Source": [],  # TODO
+                }
+            )
+            counter += 1
+
+        # Write data and validate
+        args.writer.write(
+            **{
+                "ValueTable": values
+            }
+        )
+
+        """
+        # from csvw.dsv_dialects import Dialect
+        for row in self.raw_dir.read_csv(
+                'segbo_with_glottolog.csv',
+                dicts=True,
         ):
             print(row)
             args.writer.objects['ValueTable'].append({
@@ -44,3 +84,4 @@ class Dataset(BaseDataset):
                 'Parameter_ID': '1A',
                 'Value': row['description'],
             })
+        """
